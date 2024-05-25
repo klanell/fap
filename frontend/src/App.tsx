5fbd5601c2f333479bf74d1abd9d9ddea06761f1
Jannik Hübschmann <jannik@huebschmann-online.de>
2024-05-25 14:09:50 +0200

    Login Page erster Wurf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import MapComponent from "./map/MapComponent";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./login/Login";
+import ReactDOM from "react-dom/client";
 
 function App() {
     return (
 
-        <MapComponent/>
+        <div className={"app"}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MapComponent/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
+
 
 export default App;
