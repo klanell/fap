96f24a437799c633c92adb9a71918f0177cb9482
Jannik Hübschmann <jannik@huebschmann-online.de>
2024-05-25 16:02:11 +0200

    Multiline Registration

--- frontend/src/login/register/RegisterForm.tsx
+++ frontend/src/login/register/RegisterForm.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import '../loginForm/LoginForm.css';
+import './RegisterForm.css';
+
 import {FaLock, FaUser} from "react-icons/fa";
-import { TbPassword } from "react-icons/tb";
+import {TbPassword} from "react-icons/tb";
 
 const RegisterForm = () => {
     return (
-        <div className="wrapper">
+        <div className="register-wrapper">
             <form action="">
                 <h1>Register</h1>
                 <h2>to Friends and Places</h2>
                 <div className="input-box">
-                    <input type="text" placeholder="Username" required/>
-                    <FaUser className='icon'/>
+                    <div className="input-container">
+                        <input type="text" placeholder="Username" required/>
+                        <FaUser className='icon'/>
+                    </div>
+                    <div className="input-container">
+                        <input type="text" placeholder="Username" required/>
+                        <FaUser className='icon'/>
+                    </div>
                 </div>
+
                 <div className="input-box">
                     <input type="password" placeholder="Password" required/>
                     <FaLock className='icon'/>
                 </div>
-                    <div className="input-box">
-                        <input type="password" placeholder="Confirm Password" required/>
-                        <TbPassword className='icon'/>
+                <div className="input-box">
+                    <input type="password" placeholder="Confirm Password" required/>
+                    <TbPassword className='icon'/>
                 </div>
                 <div className="remember-forgot">
                     <label><input type="checkbox"/>Remember me</label>
