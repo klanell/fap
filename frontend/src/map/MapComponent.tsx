46a3600122a2ce1a6ffe171106332f221d7baa9f
klara <klara.hoppe@gmx.de>
2024-05-25 14:19:51 +0200

    Map angepasst

--- frontend/src/map/MapComponent.tsx
+++ frontend/src/map/MapComponent.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "leaflet/dist/leaflet.css";
 import {MapContainer, Popup, TileLayer, Marker, useMap} from 'react-leaflet'
+import icon from "leaflet/dist/images/marker-icon.png";
+import L from "leaflet";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconAnchor: [13, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function MapComponent() {
-    return (
+    const [marker, setMarker] = useState<{ breitengrad: number, laengengrad: number }>();
+    useEffect(() => {
+        fetch("http://localhost:8080/FAPServer/service/fapservice/getStandort?login=tester&session=6443ca6b-b511-438d-90c0-d23c4932d0c4&id=tester").then(res => res.json()).then((res: {
+            breitengrad: number,
+            laengengrad: number
+        }) => {
+            setMarker(res);
+        }).catch(error => {
+            console.log(error);
+        })
+    });
 
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{height: "20vh"}}>
+    return (
+        <MapContainer center={[51.9481, 10.26517]} zoom={6} scrollWheelZoom={true}
+                      style={{height: "50rem", width: "80rem"}}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br/> Easily customizable.
-                </Popup>
-            </Marker>
+            {
+                marker ?
+                    <Marker position={[marker.breitengrad, marker.laengengrad]}>
+                        <Popup>
+                            Klara ist hier
+                        </Popup>
+                    </Marker> : null
+            }
+
         </MapContainer>
     );
 }
